9862f2d17138eb416d1a3e45593a79521ab4d64e
Jun Ren <chanjunren@gmail.com>
2022-06-18 17:03:33 +0800

    Removed log statement

--- src/pages/projects/proximitydetection.js
+++ src/pages/projects/proximitydetection.js
@@ -3,7 +3,6 @@ import TemplatePage from '../../components/projects/template';
 import { completedProjects } from '../../data/project_data';
 
 const ProximityDetection = () => {
-  console.log("Hello");
   return (
     <TemplatePage {...completedProjects.get('/proximitydetection')}/>
   )
