cbd3675d0d44c308a0f6e6d31e67f818a2441598
junren.chan <junren.chan@okg.com>
2024-01-30 18:28:10 +0800

    :sparkles: Changed lightIcon

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,13 +77,13 @@ const config = {
         disableSwitch: false,
         respectPrefersColorScheme: false,
         switchConfig: {
+          lightIcon: "💡",
           darkIcon: "🌙",
           darkIconStyle: {
             marginLeft: "2px",
           },
           // Unicode icons such as '\u2600' will work
           // Unicode with 5 chars require brackets: '\u{1F602}'
-          lightIcon: "\u{1F602}",
           lightIconStyle: {
             marginLeft: "1px",
           },
