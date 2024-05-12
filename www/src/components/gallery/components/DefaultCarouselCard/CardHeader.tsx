caa5ba97e6326b5172ea25c66018b17f2ecffb58
chanjunren <chanjunren@gmail.com>
2024-05-12 17:14:47 +0800

    :children_crossing: Hover touchup

--- www/src/components/gallery/components/DefaultCarouselCard/CardHeader.tsx
+++ www/src/components/gallery/components/DefaultCarouselCard/CardHeader.tsx
@@ -7,7 +7,7 @@ import { GalleryProject } from "../../types";
 const CardHeader: FC<GalleryProject> = ({ tags, id }) => {
   return (
     <header className="group/tags">
-      <Tooltip.Provider>
+      <Tooltip.Provider delayDuration={0}>
         <Tooltip.Root>
           <Tooltip.Trigger asChild>
             <button
