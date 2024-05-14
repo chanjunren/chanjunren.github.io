66ecc6f518b197565bf598fc7bc8fb9f832c3892
chanjunren <chanjunren@gmail.com>
2024-05-15 02:38:19 +0800

    :fire: RIP website_v1 you will be missed

--- website/src/utils/project_types.js
+++
@@ -1,5 +0,0 @@
-export const WEB_APP = "Website";
-export const MOBILE_APP = "Mobile App";
-export const DESKTOP_APP = "Desktop App";
-export const ANDROID_APP = "Android App";
-export const OTHER_APP = "Misc. App";
