cbf06e0718a4350866dc42878ad945b70a68a29c
junren.chan <junren.chan@okg.com>
2024-06-03 00:15:38 +0800

    :sparkles: Touched up portal

--- www/src/components/gallery/projects/portal/index.jsx
+++ www/src/components/gallery/projects/portal/index.jsx
@@ -1,34 +1,34 @@
 import { Canvas } from "@react-three/fiber";
-import { useControls } from "leva";
 import Portal from "./Portal";
 
 export default function PortalWrapper() {
-  const { position } = useControls("camera", {
-    position: {
-      value: [3, 1, 7],
-      step: 0.01,
-    },
-    // color: "#ff0000",
-    // visible: true,
-    // myInterval: {
-    //   min: 0,
-    //   max: 10,
-    //   value: [4, 5],
-    // },
-    // clickMe: button(() => {
-    //   console.log("ok");
-    // }),
-    // choice: { options: ["a", "b", "c"] },
-  });
+  // const { position } = useControls("camera", {
+  //   position: {
+  //     value: [3, 1, 7],
+  //     step: 0.01,
+  //   },
+  // color: "#ff0000",
+  // visible: true,
+  // myInterval: {
+  //   min: 0,
+  //   max: 10,
+  //   value: [4, 5],
+  // },
+  // clickMe: button(() => {
+  //   console.log("ok");
+  // }),
+  // choice: { options: ["a", "b", "c"] },
+  // });
 
   return (
     <Canvas
+      className="rounded-lg"
       flat
       camera={{
         fov: 45,
         near: 0.1,
         far: 200,
-        position: position,
+        position: [3, 1, 7],
       }}
     >
       <Portal />
