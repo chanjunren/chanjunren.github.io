3ee68aa8b8d66aa89162a3865df177d25f3f7e22
junren.chan <junren.chan@okg.com>
2024-06-05 18:27:53 +0800

    :memo: Added spel / mvel / r3f_mouse pointer event docs

---
+++ www/src/components/home/WorkExperience/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const WorkExperience: FC = () => {
+  return (
+    <div>
+      <header>💼 Experiences</header>
+    </div>
+  );
+};
+
+export default WorkExperience;
