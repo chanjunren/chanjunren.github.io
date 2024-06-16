13482b6c4d3aea72b027fa551a1bef1b25dfbdbe
chanjunren <chanjunren@gmail.com>
2024-06-16 20:50:18 +0800

    :sparkles: Touched up layout

--- www/src/pages/spotlight.tsx
+++ www/src/pages/spotlight.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import BuildingInProgress from "../components/about/BuildingInProgress";
-import LayoutWrapper from "../components/common/LayoutWrapper";
 import useGallery from "../components/home/hooks/useGallery";
 import ProjectCards from "../components/home/projects/ProjectCards";
 import ProjectInfo from "../components/spotlight/ProjectInfo";
@@ -9,30 +8,30 @@ const Spotlight: FC = () => {
   const { selectedProject } = useGallery();
 
   return (
-    <LayoutWrapper
-      title={"spotlight"}
-      description="Hello! Welcome to my digital garden"
-    >
-      <div className="flex gap-10 flex-grow">
-        <ProjectCards />
-        {/* <Separator
+    // <LayoutWrapper
+    //   title={"spotlight"}
+    //   description="Hello! Welcome to my digital garden"
+    // >
+    <div className="flex gap-10 flex-grow p-10">
+      <ProjectCards />
+      {/* <Separator
           decorative
           className="separatorRoot"
           // style={{ margin: "15px 0" }}
           orientation="vertical"
         /> */}
-        {selectedProject !== null ? (
-          <div className="flex-grow flex flex-col">
-            <ProjectInfo {...selectedProject} />
-            <div className="flex-grow max-w-screen">
-              <selectedProject.mainDisplay />
-            </div>
+      {selectedProject !== null ? (
+        <div className="flex-grow flex flex-col">
+          <ProjectInfo {...selectedProject} />
+          <div className="flex-grow max-w-screen">
+            <selectedProject.mainDisplay />
           </div>
-        ) : (
-          <BuildingInProgress />
-        )}
-      </div>
-    </LayoutWrapper>
+        </div>
+      ) : (
+        <BuildingInProgress />
+      )}
+    </div>
+    // </LayoutWrapper>
   );
 };
 
