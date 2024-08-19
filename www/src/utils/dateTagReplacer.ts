5f5b5fbb2cf86b42c17db18641c40ad879cef7b1
junren.chan <junren.chan@okg.com>
2024-08-19 23:17:42 +0800

    ✨ Minor fixes

---
+++ www/src/utils/dateTagReplacer.ts
@@ -0,0 +1,18 @@
+import { Find, Replace } from "mdast-util-find-and-replace";
+import { u } from "unist-builder";
+
+const DATE_TAG_REGEX = /(🗓️\s*\d{8}\s*\d{4}\s*\n)/g;
+
+const dateTagTransformer: [Find, Replace?] = [
+  DATE_TAG_REGEX,
+  function (input) {
+    return [
+      u("text", {
+        value: input,
+      }),
+      u("break"),
+    ];
+  },
+];
+
+export default dateTagTransformer;
