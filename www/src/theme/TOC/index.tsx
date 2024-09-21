1d3b33f6a7a116039372277bb36cfb235935d481
chanjunren <chanjunren@gmail.com>
2024-09-21 23:30:09 +0800

    :sparkles: Vaultusaurus integration

---
+++ www/src/theme/TOC/index.tsx
@@ -0,0 +1,15 @@
+import type { WrapperProps } from "@docusaurus/types";
+import TOC from "@theme-original/TOC";
+import LocalGraph from "@theme/LocalGraph";
+import type TOCType from "@theme/TOC";
+
+type Props = WrapperProps<typeof TOCType>;
+
+export default function TOCWrapper(props: Props): JSX.Element {
+  return (
+    <>
+      <LocalGraph />
+      <TOC {...props} />
+    </>
+  );
+}
