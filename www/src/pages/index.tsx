75bbf8c6c89edf15785793c42a983c38e1f3af2e
chanjunren <chanjunren@gmail.com>
2024-09-24 09:43:11 +0800

    :lipstick: Combined about section with home page

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,15 +1,28 @@
+import { ReactElement } from "react";
 import LayoutWrapper from "../components/common/LayoutWrapper";
+import Connect from "../components/home/Connect";
+import Education from "../components/home/Education";
+import Hobbies from "../components/home/Hobbies";
 import ProjectCards from "../components/home/projects/ProjectCards";
 import WelcomeSection from "../components/home/Welcome";
+import Work from "../components/home/Work";
 
-export default function Home(): JSX.Element {
+export default function Home(): ReactElement {
   return (
     <LayoutWrapper
       title={"home"}
       description="Hello! Welcome to my digital garden"
     >
-      <div className="grid xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5 items-start justify-items-center">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5 items-start justify-items-start">
         <WelcomeSection />
+        <section>
+          <Work />
+          <Education />
+        </section>
+        <section>
+          <Hobbies />
+          <Connect />
+        </section>
         <ProjectCards />
       </div>
     </LayoutWrapper>
