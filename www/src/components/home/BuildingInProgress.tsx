05cb3111224aa8210f722d6d98b4d5a3bcb58a2e
junren.chan <junren.chan@okg.com>
2024-09-27 22:12:55 +0800

    :sparkles: Minor tweaks

--- www/src/components/home/BuildingInProgress.tsx
+++ www/src/components/home/BuildingInProgress.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
+import TypewriterText from "../common/TypewriterText";
 
 const BuildingInProgress: FC = () => {
   return (
-    <span className="flex-grow mt-[10%] font-mono after:content-['|'] after:animate-blink after:bg-[var(--ifm-font-color-base)]">
-      <span className="mr-2 text-2xl">🔨</span>
-      Building in progress...
-    </span>
+    // <span className="flex-grow mt-[10%] font-mono after:content-['|'] after:animate-blink after:bg-[var(--ifm-font-color-base)]">
+    <div className="flex-grow flex items-center">
+      <TypewriterText text="🔨 Building in progress..." active />
+    </div>
+    // </span>
   );
 };
 
--- www/src/components/projects/portal/Portal.jsx
+++ www/src/components/projects/portal/Portal.jsx
@@ -3,17 +3,18 @@ import PortalScene from "./PortalScene";
 
 export default function Portal() {
   return (
-    <Canvas
-      className="rounded-lg"
-      flat
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [3, 1, 7],
-      }}
-    >
-      <PortalScene />
-    </Canvas>
+    <div className="h-[32rem]">
+      <Canvas
+        flat
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [3, 1, 7],
+        }}
+      >
+        <PortalScene />
+      </Canvas>
+    </div>
   );
 }
