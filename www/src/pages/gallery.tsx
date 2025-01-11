d34cb1a8021fcec600fa3aa19220971ab2fb911d
junren.chan <junren.chan@okg.com>
2025-01-11 23:53:06 +0800

    ✨ Enhanced BackButton
    ✨ Oops

--- www/src/pages/gallery.tsx
+++ www/src/pages/gallery.tsx
@@ -1,12 +1,16 @@
 import { ReactElement } from "react";
-import BackButton from "../components/common/BackButton";
 import Page from "../components/common/Page";
+import RedirectButton from "../components/common/RedirectButton";
 import ProjectGallery from "../components/projects/ProjectGallery";
 
 export default function Gallery(): ReactElement {
   return (
     <Page title={"gallery"} description="Hello! Welcome to my digital garden">
-      <BackButton className="md:col-span-2 self-start mb-5" />
+      <RedirectButton
+        className="md:col-span-2 self-start mb-5"
+        label="home"
+        path="/"
+      />
       <div className="flex-grow grid grid-cols-12 items-start self-center gap-10">
         <ProjectGallery />
       </div>
--- www/src/pages/spotlight.tsx
+++ www/src/pages/spotlight.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import Page from "../components/common/Page";
+import RedirectButton from "../components/common/RedirectButton";
 import BuildingInProgress from "../components/home/BuildingInProgress";
 import useGallery from "../components/home/hooks/useGallery";
 import ProjectInfo from "../components/spotlight/ProjectInfo";
@@ -9,6 +10,11 @@ const Spotlight: FC = () => {
 
   return (
     <Page title={"spotlight"} description="Hello! Welcome to my digital garden">
+      <RedirectButton
+        className="self-start mb-5"
+        label="gallery"
+        path="/gallery"
+      />
       {selectedProject !== null ? (
         <div className="flex-grow flex flex-col w-full p-10 justify-start gap-10">
           <selectedProject.banner />
