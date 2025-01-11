9a275f70527346563bf99acdaae3cf24ac50cb0c
junren.chan <junren.chan@okg.com>
2025-01-12 00:02:26 +0800

    ✨ Minor updates

--- www/src/components/common/RedirectButton.tsx
+++ www/src/components/common/RedirectButton.tsx
@@ -9,7 +9,7 @@ interface IBackButton {
 const RedirectButton: FC<IBackButton> = ({ className, label, path }) => {
   return (
     <a
-      className={`!no-underline !text-[var(--ifm-font-color-base)] text-center flex items-center w-fit ${className} hover:bg-[var(--gray-transparent-bg)] p-2 rounded-md transition-all`}
+      className={`!no-underline !text-[var(--ifm-font-color-base)] text-center flex items-center w-fit ${className} hover:bg-[var(--gray-transparent-bg)] py-2 px-4 rounded-md transition-all`}
       href={path}
       target={"_self"}
     >
