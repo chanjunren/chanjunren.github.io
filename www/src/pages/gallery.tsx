251f2586e4c956f273903527a43e00fe1c59cd55
junren.chan <junren.chan@okg.com>
2025-01-15 16:22:43 +0800

    :lipstick: Css update

--- www/src/pages/gallery.tsx
+++ www/src/pages/gallery.tsx
@@ -11,7 +11,7 @@ export default function Gallery(): ReactElement {
         label="home"
         path="/"
       />
-      <div className="flex-grow grid grid-cols-3 items-start self-center gap-10 md:px-40 md:py-10 py-5">
+      <div className="flex-grow grid grid-cols-3 items-start self-center gap-10 md:px-20 md:py-10 py-5">
         <ProjectGallery />
       </div>
     </Page>
