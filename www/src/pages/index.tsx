f80696d76682df0c403487de23fe2ba7eb285b6e
junren.chan <junren.chan@okg.com>
2025-01-28 22:16:32 +0800

    ✨ Simplify homepage

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,35 +1,31 @@
+import { ArchiveIcon, ImageIcon } from "@radix-ui/react-icons";
+import { Separator } from "@radix-ui/react-separator";
 import homeWallpaper from "@site/static/images/wallpaper.webp";
 import { ReactElement } from "react";
-import CardButton from "../components/common/CardButton";
+import HomeButton from "../components/common/HomeButton";
 import Page from "../components/common/Page";
 import Socials from "../components/home/Socials";
 export default function Home(): ReactElement {
   return (
     <Page title={"home"} description="Hello! Welcome to my digital garden">
-      <div className="flex-grow gap-3 flex flex-col md:flex-row justify-center items-center">
+      <div className="flex-grow gap-3 flex justify-center items-center relative">
         <img
-          className="rounded-lg md:w-96 md:h-96 w-80 h-72 overflow-hidden"
+          className="rounded-lg md:min-w-96 md:min-h-96 min-w-80 min-h-72 overflow-hidden absolute"
           src={homeWallpaper}
         />
-        <div className="flex flex-col gap-2 md:w-96 w-80">
-          <CardButton
-            extraProps="hover:scale-105"
-            title="About"
-            subtitle="whoami"
-            redirect="/about"
-          />
-          <CardButton
-            extraProps="hover:rotate-1"
-            redirect="/docs/zettelkasten"
-            title="zett"
+        <div className="flex flex-col gap-7 items-start justify-center md:left-80 left-48 relative">
+          <HomeButton link="/about" main={"我"} subtitle="whoami" />
+          <HomeButton
+            link="/docs/zettelkasten"
+            main={<ArchiveIcon />}
             subtitle="digital garden"
           />
-          <CardButton
-            extraProps="hover:scale-105"
-            redirect="gallery"
-            title="gallery"
-            subtitle="stuff i made"
+          <HomeButton
+            link="/gallery"
+            main={<ImageIcon />}
+            subtitle="stuff i did"
           />
+          <Separator className="separatorRoot w-5" orientation="horizontal" />
           <Socials />
         </div>
       </div>
