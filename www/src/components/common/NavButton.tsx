95631a0a5aa14fd8d8c177b3fc30cc4571994924
junren.chan <junren.chan@okg.com>
2025-02-01 00:21:35 +0800

    ✨ Update NavButton

--- www/src/components/common/NavButton.tsx
+++ www/src/components/common/NavButton.tsx
@@ -1,22 +1,23 @@
-import { ChevronLeftIcon } from "@radix-ui/react-icons";
+import { ArrowTopLeftIcon } from "@radix-ui/react-icons";
 import { FC } from "react";
-interface IBackButton {
+
+interface INavButton {
   className?: string;
   label?: string;
   path: string;
 }
 
-const RedirectButton: FC<IBackButton> = ({ className, label, path }) => {
+const NavButton: FC<INavButton> = ({ className, label, path }) => {
   return (
     <a
       className={`!no-underline !text-[var(--ifm-font-color-base)] text-center flex items-center w-fit ${className} hover:bg-[var(--gray-transparent-bg)] py-2 px-4 rounded-md transition-all`}
       href={path}
       target={"_self"}
     >
-      <ChevronLeftIcon />
+      <ArrowTopLeftIcon className="mr-2" />
       {label}
     </a>
   );
 };
 
-export default RedirectButton;
+export default NavButton;
--- www/src/pages/spotlight.tsx
+++ www/src/pages/spotlight.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
+import NavButton from "../components/common/NavButton";
 import Page from "../components/common/Page";
-import RedirectButton from "../components/common/RedirectButton";
 import BuildingInProgress from "../components/home/BuildingInProgress";
 import useGallery from "../components/home/hooks/useGallery";
 import ProjectInfo from "../components/spotlight/ProjectInfo";
@@ -14,7 +14,7 @@ const Spotlight: FC = () => {
       description="Hello! Welcome to my digital garden"
       className="lg:max-w-5xl flex flex-col justify-start gap-10"
     >
-      <RedirectButton className="self-start" label="gallery" path="/gallery" />
+      <NavButton className="self-start" label="gallery" path="/gallery" />
       {selectedProject !== null ? (
         <>
           <selectedProject.banner />
