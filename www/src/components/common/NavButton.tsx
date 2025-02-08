95d808d76af2777c6693bbf1b393b69a00169c73
chanjunren <chanjunren@gmail.com>
2025-02-09 04:15:13 +0800

    💄 Ok last touchup goodnight

--- www/src/components/common/NavButton.tsx
+++ www/src/components/common/NavButton.tsx
@@ -10,7 +10,7 @@ interface INavButton {
 const NavButton: FC<INavButton> = ({ className, label, path }) => {
   return (
     <a
-      className={`!no-underline !text-[var(--ifm-font-color-base)] text-center flex items-center w-fit ${className} hover:bg-[var(--gray-transparent-bg)] py-2 px-4 rounded-md transition-all`}
+      className={`!no-underline !text-[var(--ifm-font-color-base)] text-center flex items-center w-fit ${className} hover:bg-[var(--gray-transparent-bg)] py-3 px-3 rounded-md transition-all`}
       href={path}
       target={"_self"}
     >
