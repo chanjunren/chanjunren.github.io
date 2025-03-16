8fef3c757385a4b1e7e713aa4aa8283c6f271a8e
chanjunren <chanjunren@gmail.com>
2025-03-16 16:36:57 +0800

    ✨ Add dustbin

--- www/src/components/projects/pixellab/Experiments.tsx
+++ www/src/components/projects/pixellab/Experiments.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import Align from "./experiments/align";
+import Dustbin from "./experiments/dustbin";
 import Handwave from "./experiments/handwave";
-import PaperPlane from "./experiments/paperplane";
 
 const Experiments: FC = () => {
   return (
     <div className="grid grid-cols-5">
       <Handwave />
+      {/* <PaperPlane /> */}
       <Align />
-      <PaperPlane />
+      <Dustbin />
     </div>
   );
 };
---
+++ www/src/components/projects/pixellab/experiments/dustbin/index.tsx
@@ -0,0 +1,35 @@
+import { FC, SVGProps } from "react";
+import ExperimentBackground from "../../helpers/ExperimentBackground";
+import styles from "./index.module.css";
+
+const DustbinIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg {...props} viewBox="0 0 32 32">
+      <g
+        stroke-linejoin="round"
+        stroke-linecap="round"
+        stroke="currentColor"
+        stroke-width="0.8"
+        fill="none"
+      >
+        <g className="body">
+          <polygon points="10 12, 11 24, 21 24, 22 12" />
+          <line x1="18" y1="15" x2="18" y2="21" />
+          <line x1="14" y1="15" x2="14" y2="21" />
+        </g>
+        <g className={styles.lid}>
+          <line x1="9" y1="12" x2="23" y2="12" />
+          <rect width="4" height="2.5" x="14" y="9.5" />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default function Dustbin() {
+  return (
+    <ExperimentBackground className={styles.dustbin}>
+      <DustbinIcon width={40} />
+    </ExperimentBackground>
+  );
+}
