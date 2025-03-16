f9a0d51932cf4a287a6627e559a29cf4abd03125
junren.chan <junren.chan@okg.com>
2025-03-16 18:29:02 +0800

    ✨ Ugly airplane

--- www/src/components/projects/pixellab/Experiments.tsx
+++ www/src/components/projects/pixellab/Experiments.tsx
@@ -2,12 +2,13 @@ import { FC } from "react";
 import Align from "./experiments/align";
 import Dustbin from "./experiments/dustbin";
 import Handwave from "./experiments/handwave";
+import PaperPlane from "./experiments/paperplane";
 
 const Experiments: FC = () => {
   return (
     <div className="grid grid-cols-5">
       <Handwave />
-      {/* <PaperPlane /> */}
+      <PaperPlane />
       <Align />
       <Dustbin />
     </div>
