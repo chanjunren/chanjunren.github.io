13fff3f20dc74ecf00542c7370826b88b216a890
junren.chan <junren.chan@okg.com>
2025-03-16 15:33:59 +0800

    ✨ Wip

--- www/src/components/projects/pixellab/Experiments.tsx
+++ www/src/components/projects/pixellab/Experiments.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Align from "./experiments/align";
 import Handwave from "./experiments/handwave";
+import PaperPlane from "./experiments/paperplane";
 
 const Experiments: FC = () => {
   return (
     <div className="grid grid-cols-5">
       <Handwave />
       <Align />
+      <PaperPlane />
     </div>
   );
 };
---
+++ www/src/components/projects/pixellab/experiments/paperplane/index.tsx
@@ -0,0 +1,31 @@
+import { FC, SVGProps } from "react";
+import ExperimentBackground from "../../helpers/ExperimentBackground";
+
+const PaperPlaneIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg {...props} viewBox="0 0 64 38" width={"100%"}>
+      <g>
+        <path
+          id="Shape"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linejoin="round"
+          d="M27,22.2
+           l-4.8,10.012l8.391-7.115L22.2,32.212V20.4L61.375,1.125L22.2,20.4l-21-1.9c-0.9-0.3-1.4-1.3-1.1-2.3L60.8,0
+           c0.9,0.3,1.4,1.3,1.1,2.3L43.8,32.8c-0.3,0.9-1.3,1.4-2.3,1.1L27,22.2L61.375,1.125L27,22.2z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+const PaperPlane: FC = () => {
+  return (
+    <ExperimentBackground>
+      <PaperPlaneIcon className="p-5" />
+    </ExperimentBackground>
+  );
+};
+
+export default PaperPlane;
