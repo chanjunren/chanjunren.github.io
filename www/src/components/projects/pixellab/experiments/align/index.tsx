ea267a465524a4168e66f0521bfad2eadb2a5143
junren.chan <junren.chan@okg.com>
2025-03-16 16:36:57 +0800

    ✨ Add dustbin

--- www/src/components/projects/pixellab/experiments/align/index.tsx
+++ www/src/components/projects/pixellab/experiments/align/index.tsx
@@ -8,7 +8,7 @@ const AlignIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
         stroke-linejoin="round"
         stroke-linecap="round"
         stroke="currentColor"
-        stroke-width="1.5"
+        stroke-width="0.9"
         fill="none"
       >
         <line x1="4" y1="4" x2="4" y2="20" />
--- www/src/components/projects/pixellab/experiments/paperplane/index.tsx
+++ www/src/components/projects/pixellab/experiments/paperplane/index.tsx
@@ -1,29 +1,41 @@
-import { FC, SVGProps } from "react";
+import classnames from "classnames";
+import { FC, SVGProps, useState } from "react";
 import ExperimentBackground from "../../helpers/ExperimentBackground";
+import styles from "./index.module.css";
 
 const PaperPlaneIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg {...props} viewBox="0 0 64 38" width={"100%"}>
-      <g>
-        <path
-          id="Shape"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linejoin="round"
-          d="M27,22.2
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linejoin="round"
+        d="M27,22.2
            l-4.8,10.012l8.391-7.115L22.2,32.212V20.4L61.375,1.125L22.2,20.4l-21-1.9c-0.9-0.3-1.4-1.3-1.1-2.3L60.8,0
            c0.9,0.3,1.4,1.3,1.1,2.3L43.8,32.8c-0.3,0.9-1.3,1.4-2.3,1.1L27,22.2L61.375,1.125L27,22.2z"
-        />
-      </g>
+      />
     </svg>
   );
 };
 
 const PaperPlane: FC = () => {
+  const [active, setActive] = useState<boolean>(false);
   return (
-    <ExperimentBackground>
-      <PaperPlaneIcon className="p-5" />
+    <ExperimentBackground
+      onClick={() => {
+        if (!active) {
+          setActive(true);
+          setTimeout(() => {
+            setActive(false);
+          }, 4000);
+        }
+      }}
+    >
+      <PaperPlaneIcon
+        aria-live={active ? "assertive" : "off"}
+        className={classnames(styles.paperplane, active ? styles.active : "")}
+      />
     </ExperimentBackground>
   );
 };
