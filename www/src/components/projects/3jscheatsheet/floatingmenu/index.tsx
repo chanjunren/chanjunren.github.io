77df445be464d856b8a409da15d3691d6a72914a
junren.chan <junren.chan@okg.com>
2025-03-24 01:03:17 +0800

    ✨ Wip

--- www/src/components/projects/3jscheatsheet/floatingmenu/index.tsx
+++ www/src/components/projects/3jscheatsheet/floatingmenu/index.tsx
@@ -5,7 +5,7 @@ import {
   TokensIcon,
 } from "@radix-ui/react-icons";
 import classNames from "classnames";
-import { CSSProperties, FC, useRef } from "react";
+import { CSSProperties, FC } from "react";
 import { THREEJS_TOPIC } from "../constants";
 import useFloatingMenu from "../hooks/useFloatingMenu";
 import styles from "./index.module.css";
@@ -22,26 +22,26 @@ const topics: ThreeJsTopicInfoComponent[] = [
 ];
 
 const FloatingMenu: FC = () => {
-  const { currentTopic, setCurrentTopic, x, x2 } = useFloatingMenu();
-  const containerRef = useRef<HTMLDivElement>(null);
+  const { currentTopic, onMenuItemSelect, x1, x2, menuRef } = useFloatingMenu();
 
   return (
     <nav
-      ref={containerRef}
       style={
         {
-          "--x": x,
+          "--x1": x1,
           "--x2": x2,
         } as CSSProperties
       }
+      ref={menuRef}
       className={classNames(
-        "absolute top-28 left-1/2 transform -translate-x-1/2 flex gap-5 shadow-md p-5 rounded-lg"
+        "flex gap-5 absolute top-28 left-1/2 transform -translate-x-1/2 shadow-md p-5 rounded-3xl"
       )}
     >
+      <div className={styles.barBackground} />
       {topics.map(({ icon: Icon, key }) => (
         <button
           key={key}
-          onClick={() => setCurrentTopic(key)}
+          onClick={(e) => onMenuItemSelect(e, key)}
           className={classNames(styles.menuItem, {
             [styles.active]: currentTopic === key,
           })}
--- www/src/components/projects/3jscheatsheet/hooks/useFloatingMenu.ts
+++ www/src/components/projects/3jscheatsheet/hooks/useFloatingMenu.ts
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { THREEJS_TOPIC } from "../constants";
 
 export default function useFloatingMenu() {
   const [currentTopic, setCurrentTopic] = useState<THREEJS_TOPIC | null>();
-  const [x, setX] = useState<number>(0);
-  const [x2, setX2] = useState<number>(0);
+  const [x1, setX1] = useState<number>(20);
+  const [x2, setX2] = useState<number>(80);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // x1: (x0 - xc) / Wc
+  // x2: (x0 + w0 - xc) / Wc
+  function onMenuItemSelect(
+    e: React.MouseEvent<HTMLButtonElement>,
+    topic: THREEJS_TOPIC
+  ) {
+    const currentMenuItem = (
+      e.target as HTMLButtonElement
+    ).getBoundingClientRect();
+    const menu = menuRef.current?.getBoundingClientRect();
+    setCurrentTopic(topic);
+  }
 
   return {
     currentTopic,
-    setCurrentTopic,
-    x,
+    onMenuItemSelect,
+    menuRef,
+    x1,
     x2,
   };
 }
