b73dd043f42f5fb8adddaadfd9a46a290b2dd49a
chanjunren <chanjunren@gmail.com>
2025-04-13 02:06:43 +0800

    ✨ Small tweaks

--- www/src/components/common/PrimaryHeader.tsx
+++ www/src/components/common/PrimaryHeader.tsx
@@ -6,7 +6,7 @@ const PrimaryHeader: FC<PropsWithChildren<{ className?: string }>> = ({
 }) => {
   return (
     <h4
-      className={`text-sm uppercase font-light bg-gray-600 bg-opacity-10 backdrop-blur-2xl w-fit rounded-3xl px-5 py-3 ${className} justify-self-center md:justify-self-start`}
+      className={`text-sm uppercase font-light bg-gray-600 bg-opacity-10 backdrop-blur-2xl w-fit rounded-3xl px-5 py-3 md:justify-self-start justify-self-center ${className}`}
     >
       {children}
     </h4>
--- www/src/components/home/Gallery.tsx
+++ www/src/components/home/Gallery.tsx
@@ -5,7 +5,7 @@ import ProjectGallery from "../projects/ProjectGallery";
 export default function Gallery(): ReactElement {
   return (
     <>
-      <PrimaryHeader className="justify-self-center col-span-12 mb-5">
+      <PrimaryHeader className="!justify-self-center col-span-12 mb-5">
         🪴 gallery
       </PrimaryHeader>
       <section className="col-span-12 grid md:grid-cols-12 gap-x-4 md:gap-y-3 gap-y-5 auto-rows-[240px] md:auto-rows-[190px]">
