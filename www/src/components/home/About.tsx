016bc9eb02b93234fc4c9750a3ab4e63b91bcd7d
junren.chan <junren.chan@okg.com>
2025-04-21 00:26:09 +0800

    ♻️ Refactoring

--- www/src/components/home/About.tsx
+++ www/src/components/home/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="col-span-4 flex flex-col">
       <PrimaryHeader>👋🏻 Hi</PrimaryHeader>
       <span>I'm Jun Ren!</span>
-      <span>Welcome to my portfolio website and digital garden</span>
+      <span>Welcome to my portfolio website | digital garden</span>
     </section>
   );
 }
