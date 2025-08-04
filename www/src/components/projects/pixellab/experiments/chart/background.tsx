00ac36a964d0d20d6ff72a585d9cbadf2b793cc3
junren.chan <junren.chan@okg.com>
2025-08-05 00:33:52 +0800

    ✨ Update color

--- www/src/components/projects/pixellab/experiments/chart/background.tsx
+++ www/src/components/projects/pixellab/experiments/chart/background.tsx
@@ -4,17 +4,21 @@ export function Background({ data }) {
       <g strokeWidth="0.3">
         <polygon
           points="50,20 22,40 32,74 68,74 78,40"
-          stroke="#8A5B43"
-          fill="#0E0F13"
+          stroke="var(--ifm-font-color-base)"
+          fill="var(--ifm-background-color)"
         />
-        <g stroke="#7D7D4E" strokeDasharray="0.5" fill="none">
+        <g
+          stroke="var(--ifm-font-color-base)"
+          strokeDasharray="0.5"
+          fill="none"
+        >
           <polygon points="50,44 45,48 47,54 53,54 55,48" />
           <line x1="50" y1="20" x2="50" y2="44" />
           <line x1="22" y1="40" x2="45" y2="48" />
           <line x1="32" y1="74" x2="47" y2="54" />
           <line x1="68" y1="74" x2="53" y2="54" />
           <line x1="78" y1="40" x2="55" y2="48" />
-          <g stroke="#4F512F" strokeOpacity="0.26">
+          <g stroke="var(--ifm-font-color-base)" strokeOpacity="0.26">
             <polygon points="50,40 41,47 44,58 56,58 59,47" />
             <polygon points="50,35 36,45 41,62 59,62 64,45" />
             <polygon points="50,30 31,43 38,66 62,66 69,43" />
