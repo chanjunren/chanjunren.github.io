33a20151fec0a08b1192e8288c4cac08a01ff5db
chanjunren <chanjunren@gmail.com>
2025-11-23 15:36:50 +0800

    ✨ Minor ui touchups

--- www/src/components/home/quote/quote-info.tsx
+++ www/src/components/home/quote/quote-info.tsx
@@ -6,14 +6,17 @@ interface IQuoteInfo {
   quote: QuoteContent;
 }
 const QuoteInfo: FC<IQuoteInfo> = ({ quote }) => {
-  const { meaning, background, pinyin } = quote;
+  const { meaning, background, pinyin, sourceInfo } = quote;
 
   return (
     <div className="flex flex-col gap-3">
       <span className="text-muted-foreground">{pinyin}</span>
       <Separator />
       <span>{meaning}</span>
+      <br />
       <span>{background}</span>
+      <Separator />
+      <span>{sourceInfo}</span>
     </div>
   );
 };
